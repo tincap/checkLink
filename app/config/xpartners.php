d89ef25d3fcd8a01d1651708e8954e09d581ccde
tolik <tolik@mail.ru>
2019-02-09 16:31:32 +0300

    proxy fix

--- app/config/xpartners.php
+++ app/config/xpartners.php
@@ -4,6 +4,6 @@ return [
     'username' => 'Ayagroup',
     'password' => 'Adwtub12',
 
-    'proxy_ip' => '37.203.243.230:52753',
-    'proxy_auth' => '',
+    'proxy_ip' => '37.203.243.230:44063',
+    'proxy_auth' => 'ganiullin96_bk_ru:UJ2z2h',
 ];
